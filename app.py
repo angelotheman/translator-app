b9f71a9d9d6db698fb459b8e4d267187a9ff03b6
Kwabena Twumasi <ktwum721@gmail.com>
2022-07-22 08:52:17 +0000

    Initial Commit changes for this project I blocked all my endpoints keys from Microsoft Azure

---
+++ app.py
@@ -0,0 +1,60 @@
+from crypt import methods
+from wsgiref import headers
+from flask import Flask, redirect, url_for, request, render_template, session
+import requests, os, uuid, json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['POST'])
+def index_post():
+    original_text = request.form['text']
+    target_language = request.form['language']
+
+    # Load values from .env file
+    key = os.environ['KEY']
+    endpoint = os.environ['ENDPOINT']
+    location = os.environ['LOCATION']
+
+    # Indicate that we want to translate and the API version (3.0) and the target language
+    path = '/translate?api-version=3.0'
+
+    # Add the target parameter
+    target_language_parameter = "&to=" + target_language
+
+    # Create a full url
+    constructed_url = endpoint + path + target_language_parameter
+
+    # Set up the header information which includes our subscription key
+    headers = {
+        'Ocp-Apim-Subscription-Key': key,
+        'Ocp-Apim-Subscription-Region': location,
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # Create the body of the request with the text to be translated
+    body = [{ 'text': original_text }]
+
+    # Make the call using post
+    translator_request = requests.post(constructed_url, headers=headers, json=body)
+    # Retrieve the JSON response
+    translator_response = translator_request.json()
+    # Retrieve the translation
+    translated_text = translator_response[0]['translations'][0]['text']
+
+    # Call render template, passing the translated text,
+    # original text, and target language to the template
+    return render_template(
+        'results.html',
+        translated_text=translated_text,
+        original_text=original_text,
+        target_language=target_language
+    )
